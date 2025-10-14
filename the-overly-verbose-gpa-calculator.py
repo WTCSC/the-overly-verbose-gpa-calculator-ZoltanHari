9ea20654391e6338c36d073f59c09e5495498f37
Zoltan Hari <harizozi1009@gmail.com>
2025-10-14 14:03:51 -0600

    Start Code

--- the-overly-verbose-gpa-calculator.py
+++ the-overly-verbose-gpa-calculator.py
@@ -1,16 +1,28 @@
-while True:
-    grades_input = input("Enter all of your grades, separating them with spaces: ").split
-    try:
-        grades = [float(n) for n in grades_input.split()]
-    except ValueError:
-        print("Please enter only numbers for grades.")
-        continue
+import re
+import time
 
-    if any(g > 4.0 or g <= 0.0 for g in grades):
-        print("Please enter grades between 0.0 and 4.0.")
-        continue
-    else:
-        break
+def spell(phrase):
+    for char in phrase:
+        print(char, end = "", flush = True)
+        time.sleep(0.02)
+    time.sleep(0.2)
+
+def get_grades(prompt):
+    while True:
+        spell(prompt)
+        try:
+            grades = [float(n) for n in grades_input.split()]
+        except ValueError:
+            print("Please enter only numbers for grades.")
+            continue
+
+        if any(g > 4.0 or g <= 0.0 for g in grades):
+            print("Please enter grades between 0.0 and 4.0.")
+            continue
+        else:
+            return grades
+
+grades_input = get_grades("Enter all of your grades, separating them with spaces: ").split()
 
 print(f"Your GPA is {sum(grades) / len(grades):.2f}")
 while True:
