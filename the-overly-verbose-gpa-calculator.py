537034648e23f960a015e86e7b074e7514606d38
Zoltan Hari <harizozi1009@gmail.com>
2025-10-16 08:42:18 -0600

    Finish Code

--- the-overly-verbose-gpa-calculator.py
+++ the-overly-verbose-gpa-calculator.py
@@ -9,16 +9,16 @@ def type_out(text, delay=0.03):
     print()
 
 while True:
-    grades_input = type_out("Enter all of your grades, separating them with spaces (Ex. 3.2 4.0 2.9): ")
+    grades_input = type_out("Oi, git! Chuck in all yer gradez, wiv spacez between 'em (Ex. 3.2 4.0 2.9): ")
     grades_input = input().split()
     try:
          grades = [float(n) for n in grades_input]
     except ValueError:
-        print("Please only enter numbers for grades.")
+        print("Use numbaz, ya grot! Wot are ya, stoopid?.")
         continue
 
     if any(g > 4.0 or g <= 0.0 for g in grades):
-        print("Please only enter grades between 0.0 and 4.0.")
+        print("Da gradez gotta be between 0.0 an' 4.0, or I'll krump ya!")
         continue
     else:
         gpa = sum(grades) / len(grades)
@@ -26,61 +26,58 @@ while True:
         half_grades = len(grades_input) // 2
         break
 
-type_out(f"Your GPA for the year is {tot_gpa}")
+type_out(f"Yer total krumpin' average fer da year iz {tot_gpa}")
 while True:
-    semester = type_out("Which semester would you like to focus on? 1st Semester (first half of classes) or 2nd Semester (second half of classes):")
+    semester = type_out("Wich bit d'ya wanna looksee at? Da furst 'alf or da sekund 'alf (type 1 or 2):")
     semester = input()
-    if semester == "1" or semester == "1st":
+    if semester == "1":
         fir_gpa = sum(grades[:half_grades]) / len(grades[:half_grades])
         if fir_gpa == tot_gpa:
-            type_out("Your GPA for the 1st semester is the same as your GPA for the entire year.")
+            type_out("Yer krumpin' average fer da furst 'alf iz da same as da whole zoggin' year.")
             break
         elif fir_gpa > tot_gpa:
-            type_out(f"Your GPA for the 1st semester of {fir_gpa} is higher than your total gpa")
+            type_out(f"Yer krumpin' average fer da furst 'alf of {fir_gpa} is betta dan yer total!")
             break
         elif tot_gpa > fir_gpa:
-            type_out(f"Your GPA for the 1st semester of {fir_gpa} is lower than your total GPA")
+            type_out(f"Yer krumpin' average fer da furst 'alf of {fir_gpa} is rubbish compared ta yer total!")
             break
-    elif semester == "2" or semester == "2nd":
+    elif semester == "2":
         sec_gpa = sum(grades[half_grades:]) / len(grades[half_grades:])
         if sec_gpa == tot_gpa:
-            type_out("Your GPA for the 2nd semester is the same as your GPA for the entire year.")
+            type_out(f"Yer krumpin' average fer da sekund 'alf iz da same as da whole zoggin' year.")
             break
         elif sec_gpa > tot_gpa:
-            type_out(f"Your GPA for the 2nd semester of {sec_gpa} is higher than your total gpa")
+            type_out(f"Yer krumpin' average fer da sekund 'alf of {sec_gpa} is betta dan yer total!")
             break
         elif tot_gpa > sec_gpa:
-            type_out(f"Your GPA for the 2nd semester of {sec_gpa} is lower than your total GPA")
+            type_out(f"Yer krumpin' average fer da sekund 'alf of {sec_gpa} is rubbish compared ta yer total")
             break
     else:
-        type_out("Invalid semester choice. Please choose 1 or 2.")
+        type_out("Dat's da rong choice, ya git! Pick 1 or 2!")
         continue
 while True:
-    target_gpa_input = type_out("What is your target GPA? ")
+    target_gpa_input = type_out("Wot's da krumpin' average ya wanna get?")
     target_gpa_input = input()
     try:
         target_gpa = float(target_gpa_input)
     except ValueError:
-        type_out("Please enter a number for your target GPA.")
+        type_out("Dat 'ent a numba! Put in a proppa one!")
         continue
 
     if target_gpa > 4.0 or target_gpa <= 0.0:
-        type_out("Please enter a GPA between 0.0 and 4.0.")
+        type_out("Itz gotta be between 0.0 an' 4.0, ya stoopid git.")
         continue
     elif tot_gpa >= target_gpa:
-        type_out("You have already met your target GPA!")
+        type_out("Zog me, you'ze already done it! Dat's proppa kunnin'!")
         break
     else:
         for i in range(len(grades)):
             new_grade = grades.copy()
             new_grade[i] = 4.0
             new_gpa = sum(new_grade) / len(new_grade)
-        if new_gpa == target_gpa:
-            type_out(f"Changing grade {i+1} to 4.0 gives you a GPA of {new_gpa:.2f}, which meets your target GPA.")
-            break
-        elif new_gpa > target_gpa:
-            type_out(f"Changing grade {i+1} to 4.0 gives you a GPA of {new_gpa:.2f}, which exceeds your target GPA.")
+        if new_gpa >= target_gpa:
+            type_out(f"If ya change grade numba {i+1} ta a 4.0, ya get {new_gpa:.2f}, an' dat stomps all over yer target!")
             break
         else:
-            type_out("Changing any single grade to 4.0 will not reach your target GPA.")
+            type_out("Zog it! Changin' just wun grade ta 4.0 'ent gonna be enuff.")
             break
