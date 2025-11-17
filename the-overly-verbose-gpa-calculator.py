0389b7fd95ab0483ecd7f5c7394c2eb4f1b18737
Zoltan Hari <harizozi1009@gmail.com>
2025-11-17 07:59:23 -0700

    Add Golfed Code

--- the-overly-verbose-gpa-calculator.py
+++ the-overly-verbose-gpa-calculator.py
@@ -1,35 +1,86 @@
-import sys,time
-def type_out(t,d=.03):
-    for c in t:sys.stdout.write(c);sys.stdout.flush();time.sleep(d)
+import sys 
+import time
+
+def type_out(text, delay=0.03):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
     print()
-while 1:
-    type_out("Oi, git! Chuck in all yer gradez, wiv spacez between 'em (Ex. 3.2 4.0 2.9): ")
+
+while True:
+    grades_input = type_out("Oi, git! Chuck in all yer gradez, wiv spacez between 'em (Ex. 3.2 4.0 2.9): ")
+    grades_input = input().split()
     try:
-        g=list(map(float,input().split()))
-    except:print("Use numbaz, ya grot! Wot are ya, stoopid?.");continue
-    if any(x>4 or x<=0 for x in g):
-        print("Da gradez gotta be between 0.0 an' 4.0, or I'll krump ya!");continue
-    tot=round(sum(g)/len(g),2);h=len(g)//2;break
-type_out(f"Yer total krumpin' average fer da year iz {tot}")
-while 1:
-    type_out("Wich bit d'ya wanna looksee at? Da furst 'alf or da sekund 'alf (type 1 or 2):")
-    s=input()
-    if s in("1","2"):
-        a=g[:h]if s=="1"else g[h:]
-        r=round(sum(a)/len(a),2)
-        if r==tot:type_out(f"Yer krumpin' average fer da {'furst'if s=='1'else'sekund'} 'alf iz da same as da whole zoggin' year.");break
-        m="betta dan"if r>tot else"rubbish compared ta"
-        type_out(f"Yer krumpin' average fer da {'furst'if s=='1'else'sekund'} 'alf of {r} is {m} yer total!");break
-    type_out("Dat's da rong choice, ya git! Pick 1 or 2!")
-while 1:
-    type_out("Wot's da krumpin' average ya wanna get?")
-    try:t=float(input())
-    except:type_out("Dat 'ent a numba! Put in a proppa one!");continue
-    if t>4 or t<=0:type_out("Itz gotta be between 0.0 an' 4.0, ya stoopid git.");continue
-    if tot>=t:type_out("Zog me, you'ze already done it! Dat's proppa kunnin'!");break
-    for i in range(len(g)):
-        ng=g.copy();ng[i]=4
-        n=round(sum(ng)/len(ng),2)
-    if n>=t:type_out(f"If ya change grade numba {i+1} ta a 4.0, ya get {n:.2f}, an' dat stomps all over yer target!")
-    else:type_out("Zog it! Changin' just wun grade ta 4.0 'ent gonna be enuff.")
-    break
+         grades = [float(n) for n in grades_input]
+    except ValueError:
+        print("Use numbaz, ya grot! Wot are ya, stoopid?.")
+        continue
+
+    if any(g > 4.0 or g <= 0.0 for g in grades):
+        print("Da gradez gotta be between 0.0 an' 4.0, or I'll krump ya!")
+        continue
+    else:
+        gpa = sum(grades) / len(grades)
+        tot_gpa = round(gpa, 2)
+        half_grades = len(grades_input) // 2
+        break
+
+type_out(f"Yer total krumpin' average fer da year iz {tot_gpa}")
+while True:
+    semester = type_out("Wich bit d'ya wanna looksee at? Da furst 'alf or da sekund 'alf (type 1 or 2):")
+    semester = input()
+    if semester == "1":
+        fir_gpa = sum(grades[:half_grades]) / len(grades[:half_grades])
+        round_fir_gpa = round(fir_gpa, 2)
+        if fir_gpa == tot_gpa:
+            type_out("Yer krumpin' average fer da furst 'alf iz da same as da whole zoggin' year.")
+            break
+        elif fir_gpa > tot_gpa:
+            type_out(f"Yer krumpin' average fer da furst 'alf of {round_fir_gpa} is betta dan yer total!")
+            break
+        elif tot_gpa > fir_gpa:
+            type_out(f"Yer krumpin' average fer da furst 'alf of {round_fir_gpa} is rubbish compared ta yer total!")
+            break
+    elif semester == "2":
+        sec_gpa = sum(grades[half_grades:]) / len(grades[half_grades:])
+        round_sec_gpa = round(sec_gpa, 2)
+        if sec_gpa == tot_gpa:
+            type_out(f"Yer krumpin' average fer da sekund 'alf iz da same as da whole zoggin' year.")
+            break
+        elif sec_gpa > tot_gpa:
+            type_out(f"Yer krumpin' average fer da sekund 'alf of {round_sec_gpa} is betta dan yer total!")
+            break
+        elif tot_gpa > sec_gpa:
+            type_out(f"Yer krumpin' average fer da sekund 'alf of {round_sec_gpa} is rubbish compared ta yer total")
+            break
+    else:
+        type_out("Dat's da rong choice, ya git! Pick 1 or 2!")
+        continue
+while True:
+    target_gpa_input = type_out("Wot's da krumpin' average ya wanna get?")
+    target_gpa_input = input()
+    try:
+        target_gpa = float(target_gpa_input)
+    except ValueError:
+        type_out("Dat 'ent a numba! Put in a proppa one!")
+        continue
+
+    if target_gpa > 4.0 or target_gpa <= 0.0:
+        type_out("Itz gotta be between 0.0 an' 4.0, ya stoopid git.")
+        continue
+    elif tot_gpa >= target_gpa:
+        type_out("Zog me, you'ze already done it! Dat's proppa kunnin'!")
+        break
+    else:
+        for i in range(len(grades)):
+            new_grade = grades.copy()
+            new_grade[i] = 4.0
+            new_gpa = sum(new_grade) / len(new_grade)
+            round_new_gpa = round(new_gpa, 2)
+        if new_gpa >= target_gpa:
+            type_out(f"If ya change grade numba {i+1} ta a 4.0, ya get {round_new_gpa:.2f}, an' dat stomps all over yer target!")
+            break
+        else:
+            type_out("Zog it! Changin' just wun grade ta 4.0 'ent gonna be enuff.")
+            break
